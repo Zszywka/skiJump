d38a39990825b148aab4f33cdf835c89aa5e6119
Joanna Piotrowska <PiotrowskaWIMIIP@gmail.com>
2018-07-27 22:34:31 +0200

    Add files calculateTotalpoints.js and test

--- calculateTotalPoints.js
+++ calculateTotalPoints.js
@@ -4,8 +4,13 @@ const calculateStylePoints = require('./calculateStylePoints');
 const calculateTotalPoints = (distance, hillSize, kPoint, styleNotes, windFactor, gateFactor) => {
   const distancePoints = calculateDistancePoints(distance, hillSize, kPoint);
   const stylePoints = calculateStylePoints(styleNotes);
-
-  return distancePoints + stylePoints + windFactor + gateFactor;
+  let result = (distancePoints + stylePoints + windFactor + gateFactor);
+  //rounding function
+  function Round(result, k) {
+    var factor = Math.pow(10, k);
+    return Math.round(result * factor) / factor;
+  };
+  return Round(result, 1);
 }
 
 module.exports = calculateTotalPoints;
--- calculateTotalPoints.test.js
+++ calculateTotalPoints.test.js
@@ -1,2 +1,21 @@
 const assert = require('assert');
 const calculateTotalPoints = require('./calculateTotalPoints');
+
+describe('calculateDistancePoints', () => {
+  it('shoud return point', () => {
+    const actual = calculateTotalPoints(134.0, 'BIG', 120, [19.0, 20.0, 19.5, 19.0, 18.5], 0, (-5.4));
+
+    const expected = 137.3;
+
+    assert.equal(actual, expected);
+  });
+
+  it('shoud return point', () => {
+    const actual = calculateTotalPoints(124.0, 'BIG', 120, [17.5, 17.0, 17.5, 17.5, 17.5], 0, (-4.3));
+
+    const expected = 115.4;
+
+    assert.equal(actual, expected);
+  });
+
+});
